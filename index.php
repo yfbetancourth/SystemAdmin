09ab52ff0935c5f2178b0199c939ad312a4cf205
johnb <johnfbetancourth@gmail.com>
2023-05-30 13:08:05 -0500

    Integracion de contenedora con archivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Inicio del Sistema</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"
+    </head>
+    <body>
+        <h1 class="text-center">Hola bienvenido al mundo del desarrollo</h1>
+        <script src="js/bootstrap.min.js></script>
+    </body>
+</html>
